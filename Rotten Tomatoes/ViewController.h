9a7172173b68987e1833d6fca415a76c14675a0f
Austin Oh <austin_oh@hotmail.com>
2015-01-22 13:01:37 -0800

    Initial Commit

---
+++ Rotten Tomatoes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Rotten Tomatoes
+//
+//  Created by Austin Oh on 1/22/15.
+//  Copyright (c) 2015 Austin Oh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
